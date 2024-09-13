bf7d2f8bd1f778307ecf1eafd4f6ec5f412c507e
Juan Bautista <jbautistapulido@gmail.com>
2024-09-12 19:48:14 -0500

    Working back-end

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
 
   const config = new DocumentBuilder()
   .setTitle('Inlaze documentation')
@@ -13,8 +14,11 @@ async function bootstrap() {
   .build()
 
   const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, document)
+  SwaggerModule.setup('api', app, document,{
+    
+  }) 
 
   await app.listen(3000);
+
 }
 bootstrap();
